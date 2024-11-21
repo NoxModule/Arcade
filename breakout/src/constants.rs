5c27fc34dff74cc0b1e9f01ba2187cb2c60845ae
Christopher <ckaliszewski@gmail.com>
2024-11-21 15:20:52 -0500

    add camera & display cornflower blue

---
+++ breakout/src/constants.rs
@@ -0,0 +1,3 @@
+use bevy::color::Color;
+
+pub const BACKGROUND_COLOR: Color = Color::srgb(0.392, 0.584, 0.929);
--- breakout/src/main.rs
+++ breakout/src/main.rs
@@ -1,5 +1,21 @@
-use bevy::{app::App, DefaultPlugins};
+mod constants;
+
+use bevy::{
+    app::{App, Startup},
+    prelude::{Camera2dBundle, ClearColor, Commands},
+    DefaultPlugins,
+};
+
+use crate::constants::BACKGROUND_COLOR;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
