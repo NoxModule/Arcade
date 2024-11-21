3cc464b3f4b111b790347359de117808837692be
Christopher <ckaliszewski@gmail.com>
2024-11-21 15:40:14 -0500

    add paddle

--- breakout/src/main.rs
+++ breakout/src/main.rs
@@ -2,11 +2,16 @@ mod constants;
 
 use bevy::{
     app::{App, Startup},
-    prelude::{Camera2dBundle, ClearColor, Commands},
+    math::Vec3,
+    prelude::{Camera2dBundle, ClearColor, Commands, Transform},
+    sprite::{Sprite, SpriteBundle},
+    utils::default,
     DefaultPlugins,
 };
 
-use crate::constants::BACKGROUND_COLOR;
+use crate::constants::{
+    BACKGROUND_COLOR, BOTTOM_WALL_Y_POS, PADDLE_COLOR, PADDLE_FLOOR_GAP_SIZE, PADDLE_SIZE,
+};
 
 fn main() {
     App::new()
@@ -18,4 +23,19 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+
+    let paddle_y_pos = BOTTOM_WALL_Y_POS + PADDLE_FLOOR_GAP_SIZE;
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: PADDLE_COLOR,
+            ..default()
+        },
+        transform: Transform {
+            scale: PADDLE_SIZE.extend(1.0),
+            translation: Vec3::new(0.0, paddle_y_pos, 0.0),
+            ..default()
+        },
+        ..default()
+    });
 }
